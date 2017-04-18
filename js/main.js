cea9e05c830d533a7c8d165e4cac9b5a070e70d8
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-18 18:10:55 -0500

    create div and class

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 
-var padre = document.getElementById("body");
 window.addEventListener("load", function (e){
   e.preventDefault();
-  createDiv(3);
-  <div class="red">
-    <div class="purple">
-
-      <div class="yellow"></div>
-    </div>
-  </div>
-  <div class="blue">
-    <div class="green">
-
-    </div>
-    <div class="black"></div>
-  </div>
+  createDiv(6, document.getElementById("body"));
+  // <div class="red">
+  //   <div class="purple">
+  //     <div class="yellow"></div>
+  //   </div>
+  // </div>
+  // <div class="blue">
+  //   <div class="green"></div>
+  //   <div class="black"></div>
+  // </div>
 });
 
-function createDiv(cantidad){
-
+function createDiv(cantidad, padre){
+  var array = ["red", "purple","yellow","blue","green","black"];
+  for (var i = 0; i < cantidad; i++) {
+    var div = document.createElement("div");
+    var dcf = document.createDocumentFragment() ;
+    div.classList.add(array[i]);
+    dcf.appendChild(div);
+    padre.appendChild(dcf);
+  }
 }
