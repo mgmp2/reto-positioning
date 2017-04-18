e5f6b3a99a177d3cb4f82c8cd5dcbacc40c3f1dc
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-18 17:26:01 -0500

    ready my square

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+var padre = document.getElementById("body");
+window.addEventListener("load", function (e){
+  e.preventDefault();
+  createDiv(3);
+  <div class="red">
+    <div class="purple">
+
+      <div class="yellow"></div>
+    </div>
+  </div>
+  <div class="blue">
+    <div class="green">
+
+    </div>
+    <div class="black"></div>
+  </div>
+});
+
+function createDiv(cantidad){
+
+}
